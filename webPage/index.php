48c6cc1a0e0f52f13f2ee5f48e922208bc746684
Yuliem D. Alavez García <yuliemalavez@outlook.com>
2015-07-30 10:49:55 -0500

    Agregación de Bootstrap y página de inicio

---
+++ webPage/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Titulo</title>
+	<?php include 'inc/head_common.php'; ?>
+</head>
+<body>
+	<h1>Bootstrap ejemplo</h1>
+	<p>Hola mundo</p>
+
+	
+	<?php include 'inc/footer_common.php'; ?>
+</body>
+</html>
