bc9af3d99a68f2c7cabbacebd6c2dce72c57a313
Yuliem D. Alavez García <yuliemalavez@outlook.com>
2015-08-03 17:55:08 -0500

    Nuevos modelos para la pagina

--- webPage2/index.php
+++ webPage2/index.php
@@ -22,7 +22,7 @@
 	  		<!-- Wrapper for slides -->
 	  		<div class="carousel-inner" role="listbox">
 	    		<div class="item active">
-	      			<img src="img\facebook.png" alt="...">
+	      			<img src="img\doctores.jgg" alt="...">
 	      			<div class="carousel-caption">
 	        			...
 	      			</div>
