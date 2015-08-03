b2aeb2c8d17e08bac57ff85038c266d644d31e74
Yuliem D. Alavez García <yuliemalavez@outlook.com>
2015-08-03 10:05:12 -0500

    Termino del primer bosquejo de la página

--- webPage/inc/head_common.php
+++ webPage/inc/head_common.php
@@ -1,2 +1,4 @@
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="css/style.css">
--- webPage/index.php
+++ webPage/index.php
@@ -4,9 +4,79 @@
 	<meta charset="UTF-8">
 	<title>Mixteca Exterior</title>
 	<?php include 'inc/head_common.php'; ?>
+	<link rel="stylesheet" href="css/workshop.css">
 </head>
 <body>
 	<?php include 'inc/header.php'; ?>
+
+	<div id="main-container">
+		<article id="main-image" class="workshop workshop-left">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-8 main-info">	
+						<br>
+						<h2>Cuidamos a quienes esperan tu anhelado regreso</h2>
+						<a class="btn btn-custom" href="#">Conoce Nuestro Trabajo</a>
+						<a class="btn btn-custom" href="#">Contactanos</a>
+					</div>
+					<div class="col-xs-4"></div>
+				</div>
+			</div>
+		</article>
+		<article id="main-news" class="workshop">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+
+						<h3><span class="glyphicon glyphicon-heart"></span> Nuestra forma de apoyar</h3>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius rem quisquam pariatur enim necessitatibus ipsa tempore, magnam cum voluptatum aliquid officia ut id adipisci totam, et, saepe quaerat illum sequi.
+						</p>		
+					</div>
+					<div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+						<h3><span class="glyphicon glyphicon-user"></span> La forma como puedes apoyar</h3>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere soluta tempore voluptates omnis, consequuntur quod eaque nam eius reiciendis exercitationem! Fugiat reiciendis itaque ipsam nulla quod molestiae perferendis, voluptas placeat.
+						</p>
+
+					</div>
+					<div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+						<h3><span class="glyphicon glyphicon-thumbs-up"></span> Nuestros logros</h3>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere soluta tempore voluptates omnis, consequuntur quod eaque nam eius reiciendis exercitationem! Fugiat reiciendis itaque ipsam nulla quod molestiae perferendis, voluptas placeat.
+						</p>
+					</div>
+					<div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+						<h3><span class="glyphicon glyphicon-indent-left"></span> Nuestra historia</h3>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere soluta tempore voluptates omnis, consequuntur quod eaque nam eius reiciendis exercitationem! Fugiat reiciendis itaque ipsam nulla quod molestiae perferendis, voluptas placeat.
+						</p>
+					</div>
+				</div>
+			</div>
+		</article>
+		<article id="hospitals-partners">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12"> <h3>Hospitales o médicos que pueden brindarte atención</h3> </div>
+
+					<div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+						<img src="img/oca.png" alt="OCA" height="80" width="180">
+					</div>
+					<div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+						<img src="img/hsj.png" alt="Hospital San Jose" height="80" width="180">
+					</div>
+					<div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+						<img src="img/cm.jpg" alt="Ohristus Muguerza" height="80" width="180">
+					</div>
+					<div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+						<img src="img/oca.png" alt="OCA" height="80" width="180">
+					</div>
+				</div>
+			</div>
+		</article>
+	</div>
+	
 
 	<?php include 'inc/footer.php'; ?>
 	<?php include 'inc/footer_common.php'; ?>
