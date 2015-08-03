cb11369b9e7186e4d3270ddfe8e8b6de37612ca8
Yuliem D. Alavez García <yuliemalavez@outlook.com>
2015-08-03 00:01:08 -0500

    Agregacion de el otro bosquejo de la página web en la carpeta webPage2

---
+++ webPage2/contact.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Mixteca Exterior - Contacto</title>
+	<?php include 'inc/head_common.php'; ?>
+</head>
+<body>
+	<?php include 'inc/header.php'; ?>
+
+	<?php include 'inc/footer.php'; ?>
+	<?php include 'inc/footer_common.php'; ?>
+</body>
+</html>
