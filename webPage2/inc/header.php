e009d3166cf01bbc6aa6daf76e8a8be875eb0654
Yuliem D. Alavez García <yuliemalavez@outlook.com>
2015-08-03 01:25:20 -0500

    Agregacion del carrucel a la pagina

--- webPage2/inc/header.php
+++ webPage2/inc/header.php
@@ -1,21 +1,23 @@
-<div class="container">
-	<div class="row">
-		<div class="col-xs-3">
-			<h1 id="main-logo"><a href="index.php">Mixteca <span>Exterior</span></a></h1>
-		</div>
-		<div class="col-xs-9">
-			<ul id="main-menu" class="nav nav-pills">
-				<li class="active"><a href="index.php">Inicio</a></li>
-				<li class="dropdown">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="caret"></span>Acerca de...</a>
-					<ul class="dropdown-menu" role="menu">
-						<li><a href="detail.php">WorkShop 1</a></li>
-						<li><a href="detail.php">WorkShop 2</a></li>
-					</ul>
-				</li>
-				<li><a href="contact.php">Contáctanos</a></li>
-				<li><a href="#">Registro</a></li>					
-			</ul>
+<div id="main-header">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-2">
+				<h1 id="main-logo"><a href="index.php">Mixteca <span>Exterior</span></a></h1>
+			</div>
+			<div class="col-lg-10">
+				<ul id="main-menu" class="nav nav-pills">
+					<li class="active"><a href="index.php">INICIO</a></li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="caret"></span>CONOCENOS</a>
+						<ul class="dropdown-menu" role="menu">
+							<li><a href="detail.php">HISTORIA</a></li>
+							<li><a href="detail.php">VALORES</a></li>
+						</ul>
+					</li>
+					<li><a href="contact.php">CONTÁCTANOS</a></li>
+					<li><a href="#">REGISTRO</a></li>					
+				</ul>
+			</div>
 		</div>
 	</div>
 </div>
