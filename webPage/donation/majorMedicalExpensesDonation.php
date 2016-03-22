b52b1a68578862090d5299acc565b127f78e42dd
Yuliem D. Alavez García <yuliemalavez@outlook.com>
2016-03-21 21:18:18 -0600

    Agregación de botones de donación de paypal
    Se agregaron varios botones de paypal, para diferentes tipos de
    donaciones.

---
+++ webPage/donation/majorMedicalExpensesDonation.php
@@ -0,0 +1,19 @@
+<div class="row well">
+	<div class="col-md-3">
+		<p>
+			<b> Gastos Médicos Mayores: </b>
+			<!-- Para realizar un donativo mediante PayPal por motivo Gastos Médicos Mayores da clic en el siguiente botón -->
+		</p>
+	</div>
+	<div class="col-md-9">
+		<div class="paypal">
+			<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+				<input type="hidden" name="cmd" value="_s-xclick">
+				<input type="hidden" name="hosted_button_id" value="UYP3WNUYE7EEC">
+				<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+				<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+			</form>
+		</div>
+	</div>
+
+</div>
