a4164f884862b016661fb1ee0fe40a48b94396c4
Yuliem Alavez <yuliemalavez@outlook.com>
2016-03-29 17:28:29 -0600

    Modificacion al boton de Donativo Normal

--- webPage/donation/normalDonation.php
+++ webPage/donation/normalDonation.php
@@ -7,16 +7,11 @@
 	</div>
 	<div class="col-md-9">
 		<div class="paypal">
-			<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-				<input type="hidden" name="business" value="OSORIOROD@HOTMAIL.COM"/>
-				<input type="hidden" name="cmd" value="_xclick"/>
-				<input type="hidden" name="item_name" value="01IL001"/>
-				<input type="hidden" name="amount" value="25"/>
-				<input type="hidden" name="currency_code" value="USD"/>
-				<input type="hidden" name="shipping" value="0"/>
-				<input type="hidden" name="tax_rate" value="0"/>		
-				<input type="image" name="submit" border="0" src="https://www.paypal.com/en_US/i/btn/btn_buynowCC_LG.gif" alt="PayPal - The safer, easier way to pay online"/>
-				<img alt="" border="0" width="1" height="1" src="https://www.paypal.com/en_US/i/scr/pixel.gif"/>
+			<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+				<input type="hidden" name="cmd" value="_s-xclick">
+				<input type="hidden" name="hosted_button_id" value="D2ZD6ZGSSVZGN">
+				<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+				<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 			</form>
 		</div>
 	</div>
